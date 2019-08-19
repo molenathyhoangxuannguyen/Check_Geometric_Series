7ad7e9e7d2fd68cf06e78e49e3c50345a397f750
Thy H. Nguyen <46432072+nguyent2@users.noreply.github.com>
2019-08-19 00:14:13 -0400

    Create Geometric_series.py

---
+++ Geometric_series.py
@@ -0,0 +1,9 @@
+# Written by Thy H. Nguyen
+
+thy = input ("Enter the first number: ")
+thuy = input("Enter the second number: ")
+thuyy = input("Enter the third number:  ")
+if float(thuyy)/float(thuy) == float(thuy)/float(thy):
+    print(thy, "is the first term of the geometric series ", thuy, "is the second term of the geometric series", thuyy,2 "is the third term of the geometric series")
+else:
+    print("This is not a geometric series!!!")
