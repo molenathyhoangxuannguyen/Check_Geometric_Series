8c89614cb5b1d4783b56e256bfb14c759984233b
Thy H. Nguyen <46432072+nguyent2@users.noreply.github.com>
2019-08-20 23:31:22 -0400

    Update Geometric_series.py

--- Geometric_series.py
+++ Geometric_series.py
@@ -3,7 +3,34 @@
 thy = input ("Enter the first number: ")
 thuy = input("Enter the second number: ")
 thuyy = input("Enter the third number:  ")
-if float(thuyy)/float(thuy) == float(thuy)/float(thy):
-    print(thy, "is the first term of the geometric series ", thuy, "is the second term of the geometric series", thuyy,2 "is the third term of the geometric series")
-else:
-    print("This is not a geometric series!!!")
+try:
+    value_1 = float(thy)
+    value_2 = float(thuy)
+    value_3 = float(thuyy)
+
+    x = float(thuyy)/float(thuy)
+    y = float(thuy)/float(thy)
+
+    if x == y :
+        print(thy, "is the first term of the geometric series ",
+              thuy, "is the second term of the geometric series",
+            thuyy, "is the third term of the geometric series")
+
+        ans = input("Choose finite or infinite: ")
+        if ans == "finite":
+            print ("The finite geometric series is", thy, "+", thuy, "+", thuyy)
+            if x<1 or x>1:
+                print ("The sum of the finite geometric series is", ((value_1)*(1-x**3))/(1-x))
+            if x ==1 :
+                print ("The sum of the finite geometric series is", value_1 + value_2 + value_3)
+        if ans == "infinite" and abs(x) <1 :
+                print ("The infinite geometric series is", thy, "+", thuy, "+", thuyy, "+ ...")
+                print ("The sum of the infinite geometric series is", ((value_1)/(1-x))
+        else:
+                print ("The sum of the infinite geometric series diverges.")
+
+    else:
+        print("This is not a geometric series!!!")
+
+except ValueError:
+    print("Please enter the value again. DO NOT ENTER FRACTIONS!!! DO NOT ENTER WORDS!!!")
