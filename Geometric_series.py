1e73c470abee1964f2408447115715eabee40177
Thy H. Nguyen <46432072+nguyent2@users.noreply.github.com>
2019-08-21 10:32:30 -0400

    Update Geometric_series.py

--- Geometric_series.py
+++ Geometric_series.py
@@ -3,34 +3,37 @@
 thy = input ("Enter the first number: ")
 thuy = input("Enter the second number: ")
 thuyy = input("Enter the third number:  ")
+ans = input("finite or infinite")
 try:
-    value_1 = float(thy)
-    value_2 = float(thuy)
-    value_3 = float(thuyy)
+    float(thuy) != 0
+    float(thy) != 0
 
-    x = float(thuyy)/float(thuy)
-    y = float(thuy)/float(thy)
+    try:
+        value_1 = float(thy)
+        value_2 = float(thuy)
+        value_3 = float(thuyy)
 
-    if x == y :
-        print(thy, "is the first term of the geometric series ",
-              thuy, "is the second term of the geometric series",
-            thuyy, "is the third term of the geometric series")
+        x = float(thuyy)/float(thuy)
+        y = float(thuy)/float(thy)
 
-        ans = input("Choose finite or infinite: ")
-        if ans == "finite":
-            print ("The finite geometric series is", thy, "+", thuy, "+", thuyy)
-            if x<1 or x>1:
-                print ("The sum of the finite geometric series is", ((value_1)*(1-x**3))/(1-x))
-            if x ==1 :
-                print ("The sum of the finite geometric series is", value_1 + value_2 + value_3)
-        if ans == "infinite" and abs(x) <1 :
-                print ("The infinite geometric series is", thy, "+", thuy, "+", thuyy, "+ ...")
-                print ("The sum of the infinite geometric series is", ((value_1)/(1-x))
+        if x == y :
+            print(thy, "is the first term of the geometric series ",
+                  thuy, "is the second term of the geometric series",
+                thuyy, "is the third term of the geometric series")
         else:
-                print ("The sum of the infinite geometric series diverges.")
+            print("This is not a geometric series!!!")
 
-    else:
-        print("This is not a geometric series!!!")
+        if ans == "finite":
+            print("a")
 
-except ValueError:
-    print("Please enter the value again. DO NOT ENTER FRACTIONS!!! DO NOT ENTER WORDS!!!")
+        if ans == "infinite":
+            print("b")
+
+        if ans != "finite" and ans != "infinite":
+            print('Please choose one, and only enter "finite" or "infinite!"')
+
+    except ValueError:
+        print("Please enter the value again. DO NOT ENTER FRACTIONS!!! DO NOT ENTER WORDS!!!")
+
+except ZeroDivisionError:
+    print("cannot devide 0")
