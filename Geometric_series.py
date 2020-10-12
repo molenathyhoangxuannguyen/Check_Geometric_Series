844e6ed8d791f6eccc3e7049d3526fda2108047d
THY (MOLENA) HOANG-XUAN NGUYEN <46432072+molenathyhoangxuannguyen@users.noreply.github.com>
2020-10-11 22:24:03 -0400

    Update Geometric_series.py

--- Geometric_series.py
+++ Geometric_series.py
@@ -1,40 +1,43 @@
 # Written by Thy H. Nguyen
 
-thy = input ("Enter the first number: ")
-thuy = input("Enter the second number: ")
-thuyy = input("Enter the third number:  ")
-ans = input("finite or infinite")
-try:
-    float(thuy) != 0
-    float(thy) != 0
+num_1 = float(input ("Enter the first term of the series: "))
+num_2 = float(input("Enter the second term of the series: "))
+num_3 = float(input("Enter the third term of the series:  "))
+num_4 = input("How many total terms are considered?  ")
 
+if num_1 < num_2 < num_3 < num_4 :
     try:
-        value_1 = float(thy)
-        value_2 = float(thuy)
-        value_3 = float(thuyy)
+        float(thuy) != 0
+        float(thy) != 0
 
-        x = float(thuyy)/float(thuy)
-        y = float(thuy)/float(thy)
+        try:
+            value_1 = float(thy)
+            value_2 = float(thuy)
+            value_3 = float(thuyy)
 
-        if x == y :
-            print(thy, "is the first term of the geometric series ",
+            x = float(thuyy)/float(thuy)
+            y = float(thuy)/float(thy)
+
+            if x == y :
+                print(thy, "is the first term of the geometric series ",
                   thuy, "is the second term of the geometric series",
                 thuyy, "is the third term of the geometric series")
-        else:
-            print("This is not a geometric series!!!")
+            else:
+                print("This is not a geometric series!!!")
 
-        if ans == "finite" and x!= 1:
-            print("The sum of this finite geometric series is: ", ((value_1)*(1-x**3))/(1-x))
+            if ans == "finite" and x!= 1 and x==y:
+                print("The sum of this finite geometric series is: ", ((value_1)*(1-x**3))/(1-x))
 
+            if ans == "infinite":
+                print("b")
 
-        if ans == "infinite":
-            print("b")
+            if ans != "finite" and ans != "infinite":
+                print('Please choose one, and only enter "finite" or "infinite!"')
 
-        if ans != "finite" and ans != "infinite":
-            print('Please choose one, and only enter "finite" or "infinite!"')
+        except ValueError:
+            print("Please enter the value again. DO NOT ENTER FRACTIONS!!! DO NOT ENTER WORDS!!!")
 
-    except ValueError:
-        print("Please enter the value again. DO NOT ENTER FRACTIONS!!! DO NOT ENTER WORDS!!!")
-
-except ZeroDivisionError:
-    print("cannot devide 0")
+    except ZeroDivisionError:
+        print("The is not a geometric series. Try other methods to calculate the sum of the first n terms!")
+else:
+    print("This is not the first three terms (in a correct order) in a geometric series. Try other methods to calculate the sum of the first n terms!")
